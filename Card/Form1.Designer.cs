9a0c4e44e25791dbd51edac72aa8dfc9a10dac88
Maya <mayaredf850@ed.amdsb.ca>
2017-09-21 09:51:07 -0700

    sdf

---
+++ Card/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Card
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.Black;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(45, 66);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(315, 35);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Capricorn Greeting Card";
+            this.Title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(397, 513);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Title;
+    }
+}
+
